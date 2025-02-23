7b4ea8b07b044e41d7455896a599fcd3daaa9af9
RTNMatt <157262425+RTNMatt@users.noreply.github.com>
2025-02-22 23:31:38 -0500

    added auth. requirement to booking view set. can test with insomia/other api tools

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,10 @@
 #define URL route for index() view
 from django.urls import path
 from .views import MenuItemView, SingleMenuItemView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu/', MenuItemView.as_view(), name='menu-list-create'),
     path('menu/<int:pk>/', SingleMenuItemView.as_view(), name='menu-detail'),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,11 +2,13 @@ from rest_framework import generics
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Booking ViewSet
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access this view
 
 # List and Create Menu Items. ListCreateAPIView would be a good choice for this view if using Django Rest Framework instead of Gjango Generic Views
 class MenuItemView(generics.ListCreateAPIView):
