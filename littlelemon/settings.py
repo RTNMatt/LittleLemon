9e289857d2ecdb94c5e5ddbe355e227966a3c0d7
RTNMatt <157262425+RTNMatt@users.noreply.github.com>
2025-02-24 20:46:28 -0500

    Updated settings password to 'EnterYourPasswordHere' incase graders may not recognise one needs entered of their own

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon',
         'USER' : 'root',
-        'PASSWORD' : 'LiquidDeath!973',
+        'PASSWORD' : 'EnterYourPasswordHere',
         'HOST' : 'localhost',
         'PORT' : '3306',
         'OPTIONS': {
